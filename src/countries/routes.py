18e5803d12ca381f3e9322847e596fbc87540286
Aladeniyi Aanu <118834499+Anjola11@users.noreply.github.com>
2025-10-26 11:08:16 +0100

    Refactor country routes for improved readability
    Refactor route functions for consistency and clarity.

--- src/countries/routes.py
+++ src/countries/routes.py
@@ -10,11 +10,10 @@ from src.countries.schemas import CountryResponse, RefreshMetadataResponse
 router = APIRouter()
 db_tasks = DbUpdateDataTasks()
 
-@router.post('/refresh', status_code= status.HTTP_200_OK)
+@router.post('/refresh', status_code=status.HTTP_200_OK)
 async def add_countries(session: AsyncSession = Depends(get_session)):
-    add_countries = await db_tasks.add_countries(session)
-
-    return add_countries
+    result = await db_tasks.add_countries(session)
+    return result
 
 @router.get('/image')
 async def get_summary_image():
@@ -29,33 +28,36 @@ async def get_summary_image():
     
     return FileResponse(image_path, media_type='image/png')
 
-@router.get("", response_model= List[CountryResponse])
-@router.get("/", response_model= List[CountryResponse])
+@router.get("", response_model=List[CountryResponse])
+@router.get("/", response_model=List[CountryResponse])
 async def get_all_countries(
     region: Optional[str] = None,
     currency: Optional[str] = None,
     sort: Optional[str] = None,
-    session: AsyncSession = Depends(get_session)):
-
+    session: AsyncSession = Depends(get_session)
+):
     countries = await db_tasks.get_all_countries(region, currency, sort, session)
-
     return countries
 
 
-@router.get("/status", response_model = RefreshMetadataResponse, status_code=status.HTTP_200_OK)
+@router.get("/status", response_model=RefreshMetadataResponse, status_code=status.HTTP_200_OK)
 async def get_refresh_status(session: AsyncSession = Depends(get_session)):
     refresh_status = await db_tasks.get_refresh_status(session)
-
     return refresh_status
 
-@router.get("/{country}", status_code= status.HTTP_200_OK, response_model=CountryResponse)
-async def get_country_by_name(country_name: str,session: AsyncSession = Depends(get_session)):
-    country = await db_tasks.get_country_by_name(country_name, session)
 
+@router.get("/{country_name}", status_code=status.HTTP_200_OK, response_model=CountryResponse)
+async def get_country_by_name(
+    country_name: str,
+    session: AsyncSession = Depends(get_session)
+):
+    country = await db_tasks.get_country_by_name(country_name, session)
     return country
 
-@router.delete("/{country}", status_code= status.HTTP_200_OK)
-async def delete_country(country_name: str,session: AsyncSession = Depends(get_session)):
+@router.delete("/{country_name}", status_code=status.HTTP_200_OK)
+async def delete_country(
+    country_name: str,
+    session: AsyncSession = Depends(get_session)
+):
     deleted_country = await db_tasks.delete_country_by_name(country_name, session)
-
     return deleted_country
